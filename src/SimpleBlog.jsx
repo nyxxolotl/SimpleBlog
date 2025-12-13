3414c9e1e7a6c25474b900a5db2de84080475d52
nyxxolotl <maryannemalinao18@gmail.com>
2025-12-14 00:53:01 +0800

    Initial commit

---
+++ src/SimpleBlog.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./SimpleBlog.css";
+import mari from "./pictures/asdffwww.png";
+import 'animate.css'; 
+
+function SimpleBlog() {
+  return (
+    <div className="blog-page">
+      <div className="header">
+        <p>Home</p>
+        <p>About</p>
+        <p>Tips</p>
+      </div>
+      <div className="hero">
+        <div className="hero-text">
+          <h1>IT Habits</h1>
+          <p className="moon-dance-text">101</p>
+        </div>
+      </div>
+      <img src={mari} className="logo"/>
+      <div className="about-section">
+        <h1>About</h1>
+        <div className="about-content">
+          <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+            <p>IT Habits 101 is a blog for IT students who want to maintain mental stability while handling academic pressure, deadlines, and long hours of coding.</p>
+            <p>Inspired by the idea that small habits lead to big change, this blog shares simple, practical tips to reduce stress, prevent burnout, and build a healthier mindset—one habit at a time.</p>
+            <p>Because strong mental habits are just as important as strong technical skills.</p>
+          </div>
+          <div className="magenta-line"/>
+        </div>
+      </div>
+      <div className="tips-section">
+        <h1>Top 3 Habits for Mental Stability</h1>
+        <div className="pink-line"/>
+        <div className="tips-list">
+          <div className="top-1">
+            <h1>01</h1>
+            <div className="purple-line"/>
+            <div>
+              <h2>Build Small, Consistent Routines</h2>
+              <p>Studying, coding, and learning are mentally demanding. Creating small daily routines—like a fixed study start time or a 5-minute planning habit—reduces decision fatigue and stress. Consistency matters more than intensity.</p>
+              <button className="read-more-bttn">Read more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SimpleBlog;
